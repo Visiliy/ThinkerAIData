bec31632014b6637288816d5d85a71e904b5d3f4
Visiliy <vasiliyshkuratov_888@mail.ru>
2025-08-29 08:04:19 +0200

    /

---
+++ main.py
@@ -0,0 +1,23 @@
+from qwen_api import Qwen
+from qwen_api.core.types.chat import ChatMessage
+from dotenv import load_dotenv
+load_dotenv()
+
+
+client = Qwen()
+
+
+messages = [ChatMessage(
+    role="user",
+    content="Hello",
+    web_search=False,
+    thinking=False
+)]
+
+
+response = client.chat.create(
+    messages=messages,
+    model="qwen-max-latest"
+)
+
+print(response.choices.message.content)
